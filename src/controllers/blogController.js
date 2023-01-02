3f4f6225f6263cfd19a1e21f4879e448fdbb3b34
Anjali <anjalidelhi1509@gmail.com>
2023-01-02 15:26:59 +0530

    post completed

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,11 +1,21 @@
 const BlogModel = require("../models/blogModel")
+const AuthorModel = require("../models/authorModel")
 
 
 const createBlog = async function(req, res){
         try{
+            let authorId = req.body.authorId
+            let author = await AuthorModel.findById(authorId)
+
+            if(author){
             const data = req.body
             const allData = await BlogModel.create(data)
-            res.send(allData)
+            res.status(201).send(allData)
+            }
+            else {
+                res.status(400).send({status: false, msg : "author does not exist"})
+            }
+
             }
             catch(err){
                 res.send({status: false, msg : err.message})
@@ -14,7 +24,7 @@ const createBlog = async function(req, res){
 
  const getBlog  = async function(req, res){
     try {
-        let data = await BlogModel.find().populate("authorId")
+        let data = await (await BlogModel.find().populate("authorId")).find({isDeleted: false, isPublished: true})
         res.send(data)
     }
     catch(err) {
