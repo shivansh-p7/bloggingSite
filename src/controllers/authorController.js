8a1bccf2a79c6c452985ec20c0065522c10f1d86
Anjali <anjalidelhi1509@gmail.com>
2023-01-05 13:06:51 +0530

    changes commited

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -3,22 +3,34 @@ const AuthorModel = require("../models/authorModel");
 const createAuthor = async function (req, res) {
   try {
     const data = req.body;
-    const allData = await AuthorModel.create(data);
-    res.status(201).send(allData);
-  } catch (err) {
-    res.status(500).send({ status: false, msg: err.message });
+    let {fname, lname, title,email,password} = data
+
+    //IF ANY OF THE FIELD IS MISSING-------------------------------------------------
+    if(!fname) return res.status(400).send({status : false,msg : "cannot create author,fname is required"})
+    if(!lname) return res.status(400).send({status : false,msg : "cannot create author,lname is required"})
+    if(!title) return res.status(400).send({status : false,msg : "cannot create author,title is required"})
+    if(!email) return res.status(400).send({status : false,msg : "cannot create author, email is required"})
+    if(!password) return res.status(400).send({status : false,msg : "cannot create author,password is required"})
+
+    //IF ALL THE FIELDS ARE PROVIDED THEN CREATING THE AUTHOR----------------------------------------
+    const authorData = await AuthorModel.create(data);
+     return res.status(201).send({data: authorData});
+  } 
+  catch (err) {
+    return res.status(500).send({ status: false, msg: err.message });
   }
 };
+
+
 
 const getAuthor = async function (req, res) {
   try
  { 
   const data = await AuthorModel.find();
-  res.status(200).send(data);
+   return res.status(200).send({data : data});
 }
-
-    catch(err){
-    res.status(500).send({status:false,msg:err.message})
+ catch(err){
+   return res.status(500).send({status:false,msg:err.message})
   }
 };
 
